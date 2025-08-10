d9734628eb8445eb7e81863fa4e9314ce56c616f
yuki <eikimillian@protonmail.com>
2025-08-10 04:15:08 -0300

    adicionados endpoints e integracao com base de dados

---
+++ webapiteste/Context/AgendaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapiteste.Entities;
+
+namespace webapiteste.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options ) : base(options){}
+
+        public DbSet<Contato> Contatos { get; set; }
+
+
+    }
+}
--- webapiteste/Program.cs
+++ webapiteste/Program.cs
@@ -1,6 +1,13 @@
+using webapiteste.Entities;
+using Microsoft.EntityFrameworkCore;
+using webapiteste.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
